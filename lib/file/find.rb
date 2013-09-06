b84aafcbbbe1d46f193c0a6ba2239b679ab92acc
Daniel Berger <djberg96@gmail.com>
2013-09-06 09:48:02 -0600

    Removed rbconfig and unused variables.

--- lib/file/find.rb
+++ lib/file/find.rb
@@ -1,5 +1,4 @@
 require 'date'
-require 'rbconfig'
 
 # For alternate implementations of Ruby, such as JRuby, that cannot
 # build C extensions fall back to the Etc module.
@@ -11,7 +10,7 @@ end
 
 class File::Find
   # The version of the file-find library
-  VERSION = '0.3.5'
+  VERSION = '0.3.6'
 
   # :stopdoc:
   VALID_OPTIONS = %w/
@@ -234,7 +233,6 @@ class File::Find
             next if prune_regex.match(file)
           end
 
-          orig = file.dup
           file = File.join(path, file)
 
           stat_method = @follow ? :stat : :lstat
@@ -347,7 +345,7 @@ class File::Find
             end
           end
 
-          unless Config::CONFIG['host_os'] =~ /windows|mswin/i
+          unless File::ALT_SEPARATOR
             if @inum
               next unless stat_info.ino == @inum
             end
@@ -461,10 +459,10 @@ class File::Find
       match = cmd.match(regex)
       raise "Invalid symbolic permissions: '#{str}'" if match.nil?
 
-      junk, who, what, how = match.to_a
+      who, what, how = match.to_a[1..-1]
 
-      who  = who.split(//).inject(who_num=0){ |num,b| num |= left[b]; num }
-      how  = how.split(//).inject(how_num=0){ |num,b| num |= right[b]; num }
+      who  = who.split(//).inject(0){ |num,b| num |= left[b]; num }
+      how  = how.split(//).inject(0){ |num,b| num |= right[b]; num }
       mask = who & how
 
       case what
