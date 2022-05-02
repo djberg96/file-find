c316b257d296ff7f0d228b273c5e3501f1292013
Daniel Berger <djberg96@gmail.com>
2022-05-02 11:59:14 -0400

    Use single quotes.

--- lib/file/find.rb
+++ lib/file/find.rb
@@ -365,7 +365,7 @@ class File::Find
               octal_perm = sym2oct(@perm)
               next unless stat_info.mode & octal_perm == octal_perm
             else
-              next unless sprintf("%o", stat_info.mode & 07777) == sprintf("%o", @perm)
+              next unless sprintf('%o', stat_info.mode & 07777) == sprintf('%o', @perm)
             end
           end
 
